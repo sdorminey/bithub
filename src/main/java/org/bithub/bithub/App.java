d31bb8cec9effbf290a8436d3a1b0fade6762350
Sterling Dorminey <sterling.dorminey@gmail.com>
2014-04-27 23:17:33 -0700

    Initial commit.

---
+++ src/main/java/org/bithub/bithub/App.java
@@ -0,0 +1,13 @@
+package org.bithub.bithub;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
